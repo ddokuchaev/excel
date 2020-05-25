262de3ac4ca1d8c1eb5a78f319051a61ee1de86b
Denis Dokuchaev <d.dokuchaev@gmail.com>
2020-05-25 19:19:57 +0300

    resize v1

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,70 @@ import { createTable } from './table.template'
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    })
+    this.x = 0
+    this.y = 0
+    this.deltaX = 0
+    this.deltaY = 0
+    this.currentWidth = 0
+    this.currentHeigth = 0
+    this.onMousemove = this.onMousemove.bind(this)
+    this.onMouseup = this.onMouseup.bind(this)
+  }
+
   toHTML() {
     return createTable(30)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      console.log('start resizing', event.target.dataset.resize)
+      if (event.target.dataset.resize === 'col') {
+        // col
+        this.x = event.screenX
+        this.currentWidth = event.target.parentElement.clientWidth
+      } else {
+        // row
+        this.y = event.screenY
+        this.currentHeigth = event.target.parentElement.clientHeight
+      }
+      this.$root.on('mousemove', this.onMousemove)
+      this.$root.on('mouseup', this.onMouseup)
+    }
+  }
+
+  onMousemove(event) {
+    this.deltaX = event.screenX - this.x
+    this.deltaY = event.screenY - this.y
+    if (event.target.children.length) {
+      if (event.target.children[0].dataset.resize === 'col') {
+        // event.srcElement.style.width = this.currentWidth + this.deltaX + 'px'
+        const cellsArray = Array.from(event.target.parentElement.children)
+        const idx = cellsArray.indexOf(event.target)
+        const rows = this.$root.$el.querySelectorAll('.row')
+        // console.log('rows', rows)
+        for (let row of rows) {
+          const rowData = row.querySelectorAll('.row-data')
+          for (let cellNodeList of rowData) {
+            const cells = cellNodeList.querySelectorAll('*')
+            cells[idx].style.width = this.currentWidth + this.deltaX + 'px'
+          }
+        }
+      } else {
+        event.srcElement.style.height = this.currentHeigth + this.deltaY + 'px'
+      }
+    } else {
+      debugger
+      console.log('event.target.children.length = false')
+    }
+  }
+  onMouseup() {
+    this.$root.off('mousemove', this.onMousemove)
+    this.$root.off('mouseup', this.onMouseup)
+    this.deltaX = 0
+    this.deltaY = 0
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,7 +2,10 @@ const CODES = { A: 65, Z: 90 }
 
 function toColumn(col) {
   return `
-  <div class="column">${col}</div>
+  <div class="column">
+    ${col}
+    <div class="col-resize" data-resize="col"></div>
+  </div>
   `
 }
 
@@ -13,9 +16,15 @@ function toCell(cell) {
 }
 
 function createRow(index, content) {
+  const resizer = index
+    ? '<div class="row-resize" data-resize="row"></div>'
+    : ''
   return `
   <div class="row">
-    <div class="row-info">${index ? index : ''}</div>
+    <div class="row-info">
+      ${index ? index : ''}
+      ${resizer}
+    </div>
     <div class="row-data">${content}</div>
   </div>
   `
